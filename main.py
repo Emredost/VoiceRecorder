663d0ac4c7db6dcc2d2dbb6c5ee1e62adbf8e683
Emre Dost <138796716+Emredost@users.noreply.github.com>
2023-10-27 00:50:13 +0300

    Add files via upload
    When installing PyAudio module, you may face with "failed building wheel".  
    1- Download PyAudio from https://pypi.org/project/PyAudio/ to your Project directory
    2- Use the commands below in order to install pip PyAudio
    pip install pyaudio==0.2.11
    pip install portaudio==19.6.0

---
+++ main.py
@@ -0,0 +1,37 @@
+import pyaudio #
+import wave
+
+def record_audio(filename, duration, sample_rate=44100, channels=1):
+    audio = pyaudio.PyAudio()
+
+    stream = audio.open(format=pyaudio.paInt16,
+                        channels=channels,
+                        rate=sample_rate,
+                        input=True,
+                        frames_per_buffer=1024)
+
+    print("Recording...")
+
+    frames = []
+
+    for i in range(0, int(sample_rate / 1024 * duration)):
+        data = stream.read(1024)
+        frames.append(data)
+
+    print("Finished recording.")
+
+    stream.stop_stream()
+    stream.close()
+    audio.terminate()
+
+    with wave.open(filename, 'wb') as wf:
+        wf.setnchannels(channels)
+        wf.setsampwidth(audio.get_sample_size(pyaudio.paInt16))
+        wf.setframerate(sample_rate)
+        wf.writeframes(b''.join(frames))
+
+if __name__ == "__main__":
+    filename = "recorded_audio.wav"
+    duration = 10  # Set the recording duration in seconds
+
+    record_audio(filename, duration)
